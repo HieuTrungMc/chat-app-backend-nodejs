0835dad8e0ffbb4c2b9b022febc707fe6670e019
HieuTrungMc <phantrunghieu1109@gmail.com>
2025-04-13 17:35:31 +0700

    chore: update mysql support

--- utils/aws-helper.js
+++ utils/aws-helper.js
@@ -1,15 +1,25 @@
-const AWS = require('aws-sdk')
-require('dotenv').config()
+const AWS = require('aws-sdk');
+const mysql = require('mysql2/promise');
+require('dotenv').config();
 
 const config = new AWS.Config({
-    accessKeyId: process.env.ACCESS_KEY,
-    secretAccessKey: process.env.SECRET_KEY,
-    region: process.env.REGION,
+  accessKeyId: process.env.ACCESS_KEY,
+  secretAccessKey: process.env.SECRET_KEY,
+  region: process.env.REGION,
 })
 
 AWS.config = config;
 
-const dynamo = new AWS.DynamoDB.DocumentClient()
-const s3 = new AWS.S3()
+const s3 = new AWS.S3();
 
-module.exports = { dynamo, s3 }
+const pool = mysql.createPool({
+  host: process.env.SQL_HOST,
+  user: process.env.SQL_USER,
+  password: process.env.SQL_PASSWORD,
+  database: process.env.SQL_DATABASE,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+});
+
+module.exports = { s3, pool };
