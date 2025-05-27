e1434dc9c0593583afdae54d64a95dd35a71089c
HieuTrungMc <phantrunghieu1109@gmail.com>
2025-05-28 02:48:49 +0700

    add missing files 2

--- index.js
+++ index.js
@@ -1,45 +1,56 @@
-const express = require('express')
-const multer = require('multer')
-const cors = require('cors')
-const expressWs = require('express-ws')
-const http = require('http')
-const userRoute = require('./routes/userRoutes')
-const chatRoute = require('./routes/chatRoutes')
-const contactRoute = require('./routes/contactRoutes')
-const groupChatRoute = require('./routes/groupChatRoutes')
+const express = require('express');
+const multer = require('multer');
+const cors = require('cors');
+const fs = require('fs');
+const http = require('http');
+const https = require('https');
+const expressWs = require('express-ws');
+
+const userRoute = require('./routes/userRoutes');
+const chatRoute = require('./routes/chatRoutes');
+const contactRoute = require('./routes/contactRoutes');
+const groupChatRoute = require('./routes/groupChatRoutes');
 const callRoute = require('./routes/callRoutes')
 const setupSignalingServer = require('./service/signaling')
+const privateKey = fs.readFileSync('./cert/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('./cert/fullchain.pem', 'utf8');
+const credentials = { key: privateKey, cert: certificate };
 
-const app = express()
-const server = http.createServer(app)
-expressWs(app, server)
 
-// Set up Socket.io signaling server
+const app = express();
 const io = setupSignalingServer(server)
-
-app.use(cors())
-app.use(express.json({ extended: false }))
+app.use(cors());
+app.use(express.json({ extended: false }));
 
 app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Origin', '*');
   res.header(
       'Access-Control-Allow-Headers',
       'Origin, X-Requested-With, Content-Type, Accept'
-  )
-  next()
-})
+  );
+  next();
+});
 
-// Routes
-app.use("/user", userRoute)
-app.use("/chat", chatRoute)
-app.use("/contact", contactRoute)
-app.use("/group", groupChatRoute)
+app.use("/user", userRoute);
+app.use("/chat", chatRoute);
+app.use("/contact", contactRoute);
+app.use("/group", groupChatRoute);
 app.use("/call", callRoute)
 
-// WebSocket route - import and use it directly here
-require('./routes/websocketserver')(app)
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer(credentials, app);
 
-// Use server.listen instead of app.listen
-server.listen(process.env.PORT, () => {
-  console.log(`Server running at ${process.env.PORT}`)
-})
+expressWs(app, httpServer);
+expressWs(app, httpsServer);
+
+require('./routes/websocketserver')(app);
+
+const HTTP_PORT = process.env.PORT || 80;
+httpServer.listen(HTTP_PORT, () => {
+  console.log(`HTTP Server running on port ${HTTP_PORT} :3`);
+});
+
+const HTTPS_PORT = process.env.HTTPS_PORT || 443;
+httpsServer.listen(HTTPS_PORT, () => {
+  console.log(`HTTPS Server running on port ${HTTPS_PORT} :3`);
+});
